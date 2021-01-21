2a72abd7c60d8a58bd3231c4c8570fc58851711d
Jan Pasia <jan.pasia@gmail.com>
2021-01-21 20:42:24 +0800

    Adds page config

--- app.py
+++ app.py
@@ -6,51 +6,57 @@ import numpy as np
 # import folium
 # from streamlit_folium import folium_static
 import warnings
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
-intro = 'Introduction'
-bground = 'Background'
-dataInfo = 'Meet Our Data'
-method = 'Our Pipeline'
-eda = 'EDA'
-cluster = 'Cluster Results'
-conclusion = 'Conclusion'
-recomm = 'Recommendations'
-team = 'The Team'
-page = st.sidebar.radio('Page Navigation', [intro, bground, dataInfo, method, eda, cluster, conclusion, recomm, team])
+title = "Sprint 1 | Team No Breaks!"
+st.set_page_config(page_title=title, page_icon=":school:", layout="centered", initial_sidebar_state="collapsed")
+
+intro = "Introduction"
+bground = "Background"
+dataInfo = "Meet Our Data"
+method = "Our Pipeline"
+eda = "EDA"
+cluster = "Cluster Results"
+conclusion = "Conclusion"
+recomm = "Recommendations"
+team = "The Team"
+page = st.sidebar.radio("Page Navigation", [intro, bground, dataInfo, method, eda, cluster, conclusion, recomm, team])
 
 if page == intro:
-    st.header("Put title of presentation here")
-    st.subheader("Put subtitle here")
+  st.title("Status of school MOOE budget allocation in the Philippines in 2015")
+  st.subheader("Put subtitle here")
 
 elif page == bground:
-    st.header("Background and Context")
-    st.subheader("Put subtitle here")
+  st.title("Background and Context")
+  st.subheader("Put subtitle here")
 
 elif page == dataInfo:
-    st.header(dataInfo)
-    st.subheader("Put subtitle here")
+  st.title(dataInfo)
+  st.subheader("Put subtitle here")
 
 elif page == method:
-    st.header(method)
-    st.subheader("Put subtitle here")
+  st.title(method)
+  st.subheader("Put subtitle here")
 
 elif page == eda:
-    st.header(eda)
-    st.subheader("Put subtitle here")
+  st.title("Exploratory Data Analysis")
+  st.subheader("Put subtitle here")
 
 elif page == cluster:
-    st.header(cluster)
-    st.subheader("Put subtitle here")
+  st.title(cluster)
+  st.subheader("Put subtitle here")
 
 elif page == conclusion:
-    st.header(conclusion)
-    st.subheader("Put subtitle here")
+  st.title(conclusion)
+  st.subheader("Put subtitle here")
 
 elif page == recomm:
-    st.header(recomm)
-    st.subheader("Put subtitle here")
+  st.title(recomm)
+  st.subheader("Put subtitle here")
 
 elif page == team:
-    st.header(team)
-    st.subheader("Put subtitle here")
+  st.title(team)
+  st.header("Team No Breaks")
+  st.markdown("Mentored by Patrick Juan")
+  st.markdown("Gee-R, Nico, Pash, Tim")
+
